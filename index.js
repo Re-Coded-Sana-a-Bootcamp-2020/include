adc01562d340fd8ad0a3cd87efa273851e60a63a
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 13:13:52 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// This is a flag to indicate wethere we find what we are looking for.
+let hasBlue = false;
+
+const colors = ['red', 'yellow', 'blue', 'green'];
+
+// A sample solution using for loops might look like this.
+// for (let color of colors) {
+//   if (color === 'blue') {
+//     hasBlue = true;
+//     break;
+//   }
+// }
+
+// Create the same result as the above function. Meaning you will change the flag to true if array have the color blue.
